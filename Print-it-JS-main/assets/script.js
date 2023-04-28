76b14bb397d31ee8ed04dd018be3d7021706cf81
Mathilde Plancq <mathilde.plancq@gmail.com>
2023-04-28 11:30:35 +0200

    JS Point/images

--- Print-it-JS-main/assets/script.js
+++ Print-it-JS-main/assets/script.js
@@ -16,19 +16,45 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
-
+//-------------------------------------------------------------------------
 // Evenement d'écoute du Clique : 
 
 const leftArrow = document.querySelector('.arrow_left');
 const rightArrow = document.querySelector('.arrow_right');
 
 leftArrow.addEventListener('click', () => {
-  // code pour effectuer une tâche ici
+  // changeSlide(-1);
 	console.log("flèche gauche fonctionne"); // s'affiche dans devtool : console 
 });
 
 rightArrow.addEventListener('click', () => {
-  // code pour effectuer une tâche ici 
+  // changeSlide(1);
 	alert("flèche droite fonctionne"); // s'affiche dans une boite d'alerte
 });
+
+//-----------------------------------------------------------------------
+// compter les images du tableau slides pour afficher 1 point par image
+
+const numberOfDot = slides.length; 
+//calcul le nombre total images
+
+const dotsContainer = document.querySelector('.dots'); 
+// selection le conteneur dans HTML
+
+for (let i = 0; i < numberOfDot; i++ ){
+	const dot = document.createElement('span');
+	//creer un span pour chaque point
+
+	dot.classList.add('dot');
+	//Ajoute une class dot aux spans
+
+	dotsContainer.appendChild(dot);
+	//Ajoute span a dotsContainer. (ajoute les points sur HTML)
+}
+
+//-----------------------------------------------------------------------
+//Changer class points quand image visible
+
+let currentSlideIndex = 0; 
+// créer une variable pour savoir l'index visible
 
