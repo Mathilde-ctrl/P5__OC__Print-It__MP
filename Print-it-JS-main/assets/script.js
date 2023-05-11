2b7e0d4b40aaeca2d246b25615030d81ef781aad
Mathilde Plancq <mathilde.plancq@gmail.com>
2023-05-11 16:02:56 +0200

    Commentaire du JS

--- Print-it-JS-main/assets/script.js
+++ Print-it-JS-main/assets/script.js
@@ -17,131 +17,77 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ];
-//-------------------------------------------------------------------------
-//  Add event listeners
+// récupération de la flèche gauche
 const leftArrow = document.querySelector('.arrow_left');
+
+// récupération de la flèche droite
 const rightArrow = document.querySelector('.arrow_right');
 
-leftArrow.addEventListener('click', () => {
-  changeSlide(-1);
-});
-//When left arrow click = -1
-rightArrow.addEventListener('click', () => {
-  changeSlide(1);
-});
-//When right arrow click = 1
-
-//-------------------------------------------------------------------------
-//Create a dot for each element in the slides []
-
-const totalSlides = slides.length;
-//give the total number of elements in the slides array
-
-const dotsContainer = document.querySelector('.dot-container');
-//searches the HTML class= dot-container
-
-for (let i = 0; i < totalSlides; i++ ){
-//Loop that repeat totalSlides as long as "i" is less than "totalSlides"
-
-  const dot = document.createElement('span');
-	//create a span to represent each dot
-
-  dot.classList.add('dot');
-	//adds the class dot to the html element reprented by the dot variable
-
-	if (i === 0) {
-    dot.classList.add('dot_selected');
-  }
-	// add the "dot_selected" class to the first dot element when the page is loaded,
-  dotsContainer.appendChild(dot);
-	//appendChild() is used to add dot as a child of the dotsContainer. 
-	//That will create a new dot element for each slide and add it to the dotsContainer.
-}
-
-
-//-----------------------------------------------------------------------------------------
-//Function to change images, tagline and bullet point class when click on arrows. 
-
-let currentSlideIndex = 0;
-//initializes variable to 0 to keep track of the currient slide displayed.
-
-const dots = document.querySelectorAll('.dot');
-const image = document.querySelector('.banner-img');
-const tagline = document.querySelector('#tagline');
-//select all elements dots and first element banner-life and id tagline. 
-
-function changeSlide(direction) {
-// fonction that takes the direction parameter 
-
-  dots[currentSlideIndex].classList.remove('dot_selected'); 
-	//remove the class dot_selected
-
-  currentSlideIndex += direction;
-	//update the variable to add the value of the direction parameter 1 or -1
-	//if direction 1 advanced to the next slide. if -1 goes back. Connected with arrow value
-
-  if (currentSlideIndex < 0) {
-    currentSlideIndex = totalSlides - 1;
-		//array first element has index 0 so total number of slides in totalSlides need -1. 
-
-  } else if (currentSlideIndex >= totalSlides) {
-    currentSlideIndex = 0;
-		//goes back at the beginning of the slide when click on the last one. 
-  }
-	//
-  image.src = slides[currentSlideIndex].image;
-	//update the src of the image stored in the slides with currentSlideIndex that access the appropriate image.
-
-  tagline.innerHTML = slides[currentSlideIndex].tagLine;
-	//update the innerHtml text of the tagline stored in the slides.
-
-  dots[currentSlideIndex].classList.add('dot_selected'); 
-	//adds the class to the new current slide with currentSlideIndex. 
-}
-
-/*
-const leftArrow = document.querySelector('.arrow_left');
-const rightArrow = document.querySelector('.arrow_right');
-
+// événement d'écoute au clique avec fonction valeur -1 à exécuter 
 leftArrow.addEventListener('click', () => {
   changeSlide(-1);
 });
 
+// événement d'écoute au clique avec fonction valeur 1 à exécuter 
 rightArrow.addEventListener('click', () => {
   changeSlide(1);
 });
 
+//= 4 car nombres d'éléments dans tableau slide
 const totalSlides = slides.length;
 
+// selectionne un élement html qui à la class dot_container et assigne à la variable dotsContainer. 
 const dotsContainer = document.querySelector('.dot-container');
 
+//boucle qui va créer un span à chaque itération 
 for (let i = 0; i < totalSlides; i++ ){
-  const dot = document.createElement('span');
+
+  //créer un span et l'assigne à la variable dot
+  const dot = document.createElement('span'); 
+
+  //ajoute la classe dot à la variable dot avec classList
   dot.classList.add('dot');
+
+  //Condition si i = index 0 alors ajoute classe dot_selected
 	if (i === 0) {
     dot.classList.add('dot_selected');
   }
+  //ajoute le span comme enfant de dotsContainer. 
   dotsContainer.appendChild(dot);
 }
 
+//initialise la valeur 0 à une variable car premier element dans slide a un index 0
 let currentSlideIndex = 0;
+
+//sélectionne les éléments necessaire
 const dots = document.querySelectorAll('.dot');
 const image = document.querySelector('.banner-img');
 const tagline = document.querySelector('#tagline');
 
+//fonction avec un paramètre appelé direction
 function changeSlide(direction) {
+
+  //enlève la classe dot_selected
   dots[currentSlideIndex].classList.remove('dot_selected');
+
+  //ajoute les valeurs -1 ou 1 de direction à currentsSlideIndex
+  //met à jour l'indice de l'image 
   currentSlideIndex += direction;
 
+  //condition pour contrôler que si currentSlideIndex < 0 alors sa valeur = totalSlide -1.
+  //-1 car index de array Slide commence a 0
   if (currentSlideIndex < 0) {
     currentSlideIndex = totalSlides - 1;
+
+    //si >= 4 alors retourne à la première slide. 
+    //exemple : si click après le dernier slide alors valeur (4 >= 4)
   } else if (currentSlideIndex >= totalSlides) {
     currentSlideIndex = 0;
   }
 
+  //met à jour la source de l'image, le texte et la classe du bullet point correspondant.  
   image.src = slides[currentSlideIndex].image;
   tagline.innerHTML = slides[currentSlideIndex].tagLine;
   dots[currentSlideIndex].classList.add('dot_selected');
 }
 
-*/
